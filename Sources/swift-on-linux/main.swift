32ea6bfd37de0e61a4e285ed21a8e8401a813485
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-01-09 23:40:48 -0500

    Run some simple code

--- Sources/swift-on-linux/main.swift
+++ Sources/swift-on-linux/main.swift
@@ -1,1 +1,14 @@
-print("Hello, world!")
+import Foundation
+
+class IntroToTheApp {
+
+    func getSpecialNumber() -> Int {
+        return 42
+    }
+
+}
+
+let introToTheApp = IntroToTheApp.init()
+
+print("Created a new \(introToTheApp)")
+print("The number is \(introToTheApp.getSpecialNumber())")
